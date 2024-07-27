775c0e0b74da43780e9bb9d46d74a778395dead3
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-27 16:46:54 +0900

    ejs 세팅

--- FORUM/server.js
+++ FORUM/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 
 app.get("/", (요청, 응답) => {
   //누가 메인페이지에 접속 시
