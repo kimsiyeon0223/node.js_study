b28ffb945ccbcf0d66837a1f1f45d221f3f0e445
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-27 16:20:08 +0900

    /news에 접속시 디비에 어쩌구 값 넣어보기

--- FORUM/server.js
+++ FORUM/server.js
@@ -3,19 +3,34 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 실행중");
-});
-
 app.get("/", (요청, 응답) => {
   //누가 메인페이지에 접속 시
   응답.sendFile(__dirname + "/index.html");
 });
 
 app.get("/news", (요청, 응답) => {
-  응답.send("오늘 비옴");
+  db.collection("post").insertOne({ title: "어쩌구" });
+  // 응답.send("오늘 비옴");
 });
 
 app.get("/shop", (요청, 응답) => {
   응답.send("쇼핑페이지임");
 });
+
+const { MongoClient } = require("mongodb");
+
+let db;
+const url =
+  "mongodb+srv://admin:1234@cluster.ist8kmr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("forum_post");
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 실행중");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
